975317e52ad68ca4aaab693b2f943c8aaf7a10b2
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2025-07-08 15:14:03 +0300

    Added a custom toupper function, to make sure commands are always uppercase.

--- src/Command.cpp
+++ src/Command.cpp
@@ -6,11 +6,22 @@
 /*   By: ahentton <ahentton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 13:21:55 by ahentton          #+#    #+#             */
-/*   Updated: 2025/07/07 14:52:08 by ahentton         ###   ########.fr       */
+/*   Updated: 2025/07/08 15:13:40 by ahentton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Command.hpp"
+
+/*Simple helper function to ensure commands
+  are always stored in uppercase format.*/
+static  std::string stringToUpper(std::string cmd)
+{
+    for (size_t i = 0; i < cmd.length(); i++)
+    {
+        std::toupper(cmd[i]);
+    }
+    return (cmd);
+}
 
 /* joinTrail will pick up the token where ':' was found,
    and join it with the rest of the stringstream, which is picked up with std::getline.
@@ -29,6 +40,7 @@ static  std::string joinTrail(std::string token, std::istringstream &iss)
 /* msgToCmd uses istringstream to tokenize the message.
    The function recognizes the token as either prefix, command, or parameter.
    If prefix is missing, first token is always the command portion of the message.
+   Command is always stored in uppercase, hence the stringToupper helper function.
    If a trailing parameter is found, joinTrail joins rest of the stringstream into a single parameter.
    Thus concluding the process and returning the finished cmd structure*/
    
@@ -46,12 +58,12 @@ Command msgToCmd(std::string message)
             if (token[0] == ':')
                 cmd.prefix = token;
             else
-                cmd.command = token;
+                cmd.command = stringToUpper(token);
         }
         else if (!token.empty() && token_counter == 1)
         {
             if (!cmd.prefix.empty())
-                cmd.command = token;
+                cmd.command = stringToUpper(token);
             else
                 cmd.params.push_back(token);
         }
