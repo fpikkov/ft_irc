67b9f006544e0b64d94748889341a4e58ef27962
Ruslan <ruslankhakimullin@gmail.com>
2025-07-08 13:53:44 +0300

    WIP CommandHandler branch. Added the hpp methods to be implemented

--- include/CommandHandler.hpp
+++ include/CommandHandler.hpp
@@ -1,29 +1,54 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   CommandHandler.hpp                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ahentton <ahentton@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/08 12:50:10 by ahentton          #+#    #+#             */
-/*   Updated: 2025/07/08 13:19:27 by ahentton         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Server.hpp"
 #include "Client.hpp"
 #include "Command.hpp"
 #include <functional>
 #include <unordered_map>
-#include <string>
 
 class CommandHandler
 {
 
-    private:
-            Server  &_server;
-            void    handleCommand(Client &client, Command const &cmd);
+	private:
+			Server&	_server;
+			// std::function allows us to store any callable (lambda, function pointer, etc.). "using" is a type alias for handler functions
+			using HandleFunction = std::function<void(Client&, const Command&)>;
+			// Mapping command name with the handler function
+			std::unordered_map<std::string, HandleFunction> _handlers;
 
-    public:
-            CommandHandler(Server &server);
+			// Registration commands
+			void handlePass(Client&, const Command&);
+			void handleNick(Client&, const Command&);
+			void handleUser(Client&, const Command&);
+
+			// Message commands
+			void handlePrivmsg(Client&, const Command&);
+			void handleNotice(Client&, const Command&);
+
+			//Channel commands
+			void handleJoin(Client&, const Command&);
+			void handlePart(Client&, const Command&);
+			void handleKick(Client&, const Command&);
+			void handleInvite(Client&, const Command&);
+			void handleTopic(Client&, const Command&);
+			void handleMode(Client&, const Command&);
+			
+			// Rest of the commands
+			void handleQuit(Client&, const Command&);
+			void handlePing(Client&, const Command&);
+			void handlePong(Client&, const Command&);
+
+	public:
+			CommandHandler(Server& server);
+			void	handleCommand(Client& client, Command const& cmd);
 };
+
+
+// Minimum commands to cover
+// PASS
+// NICK
+// USER
+// QUIT
+// JOIN
+// PART
+// PRIVMSG
+// NOTICE(not sure)
+// PING
