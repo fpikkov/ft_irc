dcb216b081d365f17350fb574c3e5ccf186dc8e4
Anselmi Henttonen <AnselmiVeikko@gmail.com>
2025-07-09 16:09:06 +0300

    Replaced includes with forward class/struct declarations in the header file.

--- include/CommandHandler.hpp
+++ include/CommandHandler.hpp
@@ -1,11 +1,10 @@
 #pragma once
-#include "Server.hpp"
-#include "Client.hpp"
-#include "Command.hpp"
-#include "Response.hpp"
 #include <functional>
 #include <unordered_map>
 
+class	Server;
+class	Client;
+struct	Command;
 class CommandHandler
 {
 
